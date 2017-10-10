4cd501beecc44299b3abab77fdff1d1d99d78d8e
dongganen <dge_1992@163.com>
2017-10-10 17:11:48 +0800

    2017/10/10

--- ssm/src/main/java/com/google/ssm/SsmApplication.java
+++ ssm/src/main/java/com/google/ssm/SsmApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 
 @SpringBootApplication
--- ssm/src/main/java/com/google/ssm/web/TestController.java
+++ ssm/src/main/java/com/google/ssm/web/TestController.java
@@ -3,6 +3,7 @@ package com.google.ssm.web;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequestMapping("test")
@@ -12,5 +13,17 @@ public class TestController {
     public String returnAAA(Model model){
         model.addAttribute("name", "Mike");
         return "abc";
+    }
+
+    @RequestMapping("returnTest1")
+    public String returnTest1(){
+        return "test1";
+    }
+
+    @RequestMapping("aaa")
+    @ResponseBody
+    public Object aaa(){
+        int a = 0;
+        return "";
     }
 }
