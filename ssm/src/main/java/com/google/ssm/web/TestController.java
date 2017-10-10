4cf493f57d339f563a0fac21cb27d6adc2bb7e67
dongganen <dongganen@qnvip.com>
2017-10-10 20:52:59 +0800

    2017/10/10/2

--- ssm/src/main/java/com/google/ssm/web/TestController.java
+++ ssm/src/main/java/com/google/ssm/web/TestController.java
@@ -1,5 +1,8 @@
 package com.google.ssm.web;
 
+import com.alibaba.fastjson.JSON;
+import com.google.ssm.domain.Group;
+import com.google.ssm.domain.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,20 +13,38 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class TestController {
 
     @RequestMapping("returnAAA")
-    public String returnAAA(Model model){
+    public String returnAAA(Model model) {
         model.addAttribute("name", "Mike");
         return "abc";
     }
 
     @RequestMapping("returnTest1")
-    public String returnTest1(){
+    public String returnTest1() {
         return "test1";
     }
 
     @RequestMapping("aaa")
     @ResponseBody
-    public Object aaa(){
-        int a = 0;
-        return "";
+    public Object aaa(String username, String content) {
+        Group group = new Group();
+        group.setId(0L);
+        group.setName("admin");
+
+        User guestUser = new User();
+        guestUser.setId(2L);
+        guestUser.setName("guest");
+
+        User rootUser = new User();
+        rootUser.setId(3L);
+        rootUser.setName("root");
+
+        group.addUser(guestUser);
+        group.addUser(rootUser);
+
+        String jsonString = JSON.toJSONString(group);
+        Group group1 = JSON.parseObject(jsonString, Group.class);
+
+        System.out.println(jsonString);
+        return jsonString;
     }
 }
