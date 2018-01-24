204a95f3e44f7410f81d95657cc2cdf07372cbe1
dongganen <dge_1992@163.com>
2018-01-24 13:49:23 +0800

    清空ssm

--- ssm/sql/ssm.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE `city` (
-  `id` int(10) unsigned NOT NULL AUTO_INCREMENT COMMENT '城市编号',
-  `province_id` int(10) unsigned NOT NULL COMMENT '省份编号',
-  `city_name` varchar(25) DEFAULT NULL COMMENT '城市名称',
-  `description` varchar(25) DEFAULT NULL COMMENT '描述',
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
-
