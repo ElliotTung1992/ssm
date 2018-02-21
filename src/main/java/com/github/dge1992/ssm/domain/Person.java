3e54b92cd85bdfd2a6b174e0ed4a52da0c6d200e
dongganen <dongganen@qnvip.com>
2018-02-21 15:33:43 +0800

    jackson设置时区

--- src/main/java/com/github/dge1992/ssm/domain/Person.java
+++ src/main/java/com/github/dge1992/ssm/domain/Person.java
@@ -35,14 +35,14 @@ public class Person extends Model<Person> {
      * 创建时间
      */
 //    @DateTimeFormat(pattern="yyyy-MM-dd HH:mm:ss")
-    @JsonFormat(pattern="yyyy-MM-dd HH:mm:ss",timezone = "GMT+8")
+//    @JsonFormat(pattern="yyyy-MM-dd HH:mm:ss",timezone = "GMT+8")
     @TableField("create_time")
     private Date createTime;
     /**
      * 更新时间
      */
 //    @DateTimeFormat(pattern="yyyy-MM-dd")
-    @JsonFormat(pattern="yyyy-MM-dd HH:mm:ss",timezone = "GMT+8")
+//    @JsonFormat(pattern="yyyy-MM-dd HH:mm:ss",timezone = "GMT+8")
     @TableField("update_time")
     private Date updateTime;
     /**
